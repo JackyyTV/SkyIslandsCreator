403c5f6671dc43f92a342c2fb9d224c5d65082b4
Jacky1356400 <jacky1356400@hotmail.com>
2017-08-06 01:39:57 +0800

    *Actually* ported to 1.11.2

---
+++ src/main/java/me/jacky1356400/skyislandscreator/commands/JoinIslandCommand.java
@@ -0,0 +1,65 @@
+package me.jacky1356400.skyislandscreator.commands;
+
+import me.jacky1356400.skyislandscreator.island.IslandUtils;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JoinIslandCommand extends CommandBase implements ICommand {
+
+    private List<String> aliases;
+
+    public JoinIslandCommand() {
+        aliases = new ArrayList<>();
+        aliases.add("skyislands_join");
+        aliases.add("skyisland_join");
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return aliases;
+    }
+
+    @Override
+    public String getName() {
+        return aliases.get(0);
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 2;
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "skyislands_join <IslandName>";
+    }
+    
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] input, @Nullable BlockPos pos) {
+        return input.length == 1 ? getListOfStringsMatchingLastWord(input, server.getServer().getOnlinePlayerNames())
+                : null;
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] input) throws CommandException {
+        World world = sender.getEntityWorld();
+        EntityPlayerMP player = (EntityPlayerMP) world.getPlayerEntityByName(sender.getName());
+        if (input.length == 0) {
+            sender.sendMessage(new TextComponentString("Invalid arguments!"));
+        } else {
+            IslandUtils.joinIsland(input[0], player);
+        }
+    }
+
+}
---
+++ src/main/java/me/jacky1356400/skyislandscreator/commands/SaveIslandsCommand.java
@@ -0,0 +1,48 @@
+package me.jacky1356400.skyislandscreator.commands;
+
+import me.jacky1356400.skyislandscreator.proxy.CommonProxy;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaveIslandsCommand extends CommandBase implements ICommand {
+
+    private List<String> aliases;
+
+    public SaveIslandsCommand() {
+        aliases = new ArrayList<>();
+        aliases.add("skyislands_save");
+        aliases.add("skyisland_save");
+    }
+
+    @Override
+    public List getAliases() {
+        return aliases;
+    }
+
+    @Override
+    public String getName() {
+        return aliases.get(0);
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "skyislands_save";
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] input) throws CommandException {
+        try {
+            CommonProxy.saveIslands(CommonProxy.getIslands());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
